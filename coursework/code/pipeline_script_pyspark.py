ef2cbe8aa28ce31c74d80fb126d72124d3d9640a
Cloud User <ec2-user@ip-10-0-13-106.eu-west-2.compute.internal>
2023-11-22 00:53:29 +0000

    [cw] Save woker output in s3 bucket
    To be able to do a distributed work, the worker will save it's output
    file (after final parser) in the s3 bucket.
    After a distributed work, the master will gather all files in the s3
    folder that has the output files of the workers and put it into a single
    file.
    
    Probably, in the future, there will be a new worker that will be
    schedule to check if there's a new file available to append it to the
    total result

--- coursework/code/pipeline_script_pyspark.py
+++ coursework/code/pipeline_script_pyspark.py
@@ -3,18 +3,20 @@ import sys
 from subprocess import Popen, PIPE
 from Bio import SeqIO
 from pyspark.sql import SparkSession
-
+import boto3
+from botocore.exceptions import ClientError
+ROOT_DIR = os.path.dirname(os.path.abspath(__file__))
 
 """
 usage: python pipeline_script.py INPUT.fasta  
 approx 5min per analysis
 """
 
-def run_parser(hhr_file):
+def run_parser(hhr_file, output_file):
     """
     Run the results_parser.py over the hhr file to produce the output summary
     """
-    cmd = ['python', './results_parser.py', hhr_file]
+    cmd = ['python', './results_parser.py', hhr_file, output_file]
     print(f'STEP 4: RUNNING PARSER: {" ".join(cmd)}')
     p = Popen(cmd, stdin=PIPE,stdout=PIPE, stderr=PIPE)
     out, err = p.communicate()
@@ -87,7 +89,25 @@ def add_to_mapping(mapping_file, k, index):
     """
     with open(mapping_file, "a") as fh_out:
         fh_out.write(f"{k}\t{index}\n")
-    
+
+
+def upload_file_to_s3(bucket, file_name, object_name=None):
+    # If S3 object_name was not specified, use file_name
+    if object_name is None:
+        object_name = file_name
+
+    # Create an S3 client
+    s3_client = boto3.client('s3')
+
+    # Upload the file
+    try:
+        response = s3_client.upload_file(file_name, bucket, object_name)
+    except ClientError as e:
+        print("Error uploading file to S3")
+        return False
+    return True
+
+
 def read_input(file):
     """
     Function reads a fasta formatted file of protein sequences
@@ -106,17 +126,17 @@ def cleanup(tmp_file, horiz_file, a3m_file, hhr_file):
     lambda_rm = lambda x: os.remove(x)
     list(map(lambda_rm, [tmp_file, horiz_file, a3m_file, hhr_file]))
 
-def process_sequence(k, v, index):
-    mapping_file = "/mnt/data/DataEng1/coursework/code/tmp/mapping.txt"
-    tmp_file = f"/mnt/data/DataEng1/coursework/code/tmp/tmp/{index}.fas"
-    horiz_file = f"/mnt/data/DataEng1/coursework/code/tmp/horiz/{index}.horiz"
-    a3m_file = f"/mnt/data/DataEng1/coursework/code/tmp/a3m/{index}.a3m"
-    hhr_file = f"/mnt/data/DataEng1/coursework/code/tmp/hhr/{index}.hhr"
-
+def process_sequence(k, v, run_id, bucket, index):
+    mapping_file = f"{ROOT_DIR}/tmp/mapping.txt"
+    tmp_file = f"{ROOT_DIR}/tmp/tmp/{index}.fas"
+    horiz_file = f"{ROOT_DIR}/tmp/horiz/{index}.horiz"
+    a3m_file = f"{ROOT_DIR}/tmp/a3m/{index}.a3m"
+    hhr_file = f"{ROOT_DIR}/tmp/a3m/{index}.hhr"
+    object_name = f"{run_id}/{index}.out"
+    output_file = f"{ROOT_DIR}/output/{run_id}/{index}.out"
     os.makedirs(os.path.dirname(tmp_file), exist_ok=True)
     os.makedirs(os.path.dirname(horiz_file), exist_ok=True)
     os.makedirs(os.path.dirname(a3m_file), exist_ok=True)
-    os.makedirs(os.path.dirname(hhr_file), exist_ok=True)
 
     with open(tmp_file, "w") as fh_out:
         fh_out.write(f">{k}\n")
@@ -126,16 +146,53 @@ def process_sequence(k, v, index):
     run_s4pred(tmp_file, horiz_file)
     read_horiz(tmp_file, horiz_file, a3m_file)
     run_hhsearch(a3m_file)
-    run_parser(hhr_file)
+    run_parser(hhr_file, output_file)
+    upload_file_to_s3(bucket, output_file, object_name)
     #cleanup(tmp_file, horiz_file, a3m_file, hhr_file)
 
 
+
+def merge_results(bucket, run_id):
+    """
+    Function to merge the results from the individual runs
+    """
+    print("MERGING RESULTS")
+    s3 = boto3.resource('s3')
+    bucket = s3.Bucket(bucket)
+    results = []
+    # for all files in the folder run_id in the bucket
+    for obj in bucket.objects.filter(Prefix=f"{run_id}/"):
+        print("Reading file: ", obj.key)
+        body = obj.get()['Body'].read().decode('utf-8')
+        lines = body.split("\n")
+        results.extend(lines)
+
+    with open(f"{ROOT_DIR}/output/{run_id}.csv", "w") as fh_out:
+        fh_out.write("query_id,best_hit,best_evalue,best_score,score_mean,score_std,score_gmean\n")
+        for line in results:
+            fh_out.write(line + "\n")
+
+
 if __name__ == "__main__":
+    run_id = "run_test"
+    bucket = "comp0235-ucabfri"
+
     if len(sys.argv) != 2:
         print("Usage: python script.py input_file")
         sys.exit(1)
 
-    spark = SparkSession.builder.appName("pdb_analyse").getOrCreate()
+    # if --master is not specified, it will run locally
+    spark = None
+    if "--local" in sys.argv:
+        spark = SparkSession.builder.appName("pdb_analyse").master("local[*]").getOrCreate()
+    else:
+        spark = SparkSession.builder.appName("pdb_analyse").master("spark://ip-10-0-13-106.eu-west-2.compute.internal:7077").getOrCreate()
+    
+
+    if os.path.exists(f"{ROOT_DIR}/output/{run_id}"):
+        os.system(f"rm -rf {ROOT_DIR}/output/{run_id}")
+
+    os.makedirs(f"{ROOT_DIR}/output/{run_id}")
 
     sequences = read_input(sys.argv[1])
 
@@ -143,6 +200,8 @@ if __name__ == "__main__":
 
     parallelised_data = spark.sparkContext.parallelize(sequence_list)
 
-    parallelised_data.foreach(lambda x: process_sequence(x[0], x[1], sequence_list.index(x)))
+    parallelised_data.foreach(lambda x: process_sequence(x[0], x[1], run_id, bucket, sequence_list.index(x)))
+
+    merge_results(bucket, run_id)
 
     spark.stop()
--- coursework/code/results_parser.py
+++ coursework/code/results_parser.py
@@ -8,11 +8,13 @@ best_score = 0
 good_hit_scores  = []
 id = ''
 
-if len(sys.argv) != 2:
-    print("Usage: python results_parser.py <hhr_file>")
+if len(sys.argv) != 3:
+    print("Usage: python results_parser.py <hhr_file> <output_file>")
     sys.exit(1)
 
 hhr_file = sys.argv[1]
+output_file = sys.argv[2]
+
 for result in SearchIO.parse(hhr_file, 'hhsuite3-text'):
     id=result.id
     for hit in result.hits:
@@ -22,15 +24,14 @@ for result in SearchIO.parse(hhr_file, 'hhsuite3-text'):
         if hit.evalue < 1.e-5:
             good_hit_scores.append(hit.score)
 
-if not os.path.exists("hhr_parse.out"):
-    fhOut = open("hhr_parse.out", "w")
-    fhOut.write("query_id,best_hit,best_evalue,best_score,score_mean,score_std,score_gmean\n")
-    fhOut.close()
+if not os.path.exists(output_file):
+    os.makedirs(os.path.dirname(output_file), exist_ok=True)
 
-fhOut = open("hhr_parse.out", "a")
+fhOut = open(output_file, "w")
 mean=format(np.mean(good_hit_scores), ".2f")
 std=format(np.std(good_hit_scores), ".2f")
 g_mean=format(gmean(good_hit_scores), ".2f")
 
 fhOut.write(f"{id},{best_hit[0]},{best_hit[1]},{best_hit[2]},{mean},{std},{g_mean}\n")
 fhOut.close()
+print(f"Results written to {output_file}")
