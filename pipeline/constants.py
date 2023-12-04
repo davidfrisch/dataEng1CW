7662a143664d4974a9d14a114e2e317898c2b64c
Cloud User <ec2-user@ip-10-0-0-169.eu-west-2.compute.internal>
2023-12-04 22:27:01 +0000

    [script] Update env script

--- pipeline/constants.py
+++ pipeline/constants.py
@@ -3,8 +3,9 @@ import sys
 from dotenv import load_dotenv
 ROOT_DIR = os.path.dirname(os.path.abspath(__file__))+ "/.."
 SRC_DIR = os.path.dirname(os.path.abspath(__file__))
-#TODO change this to the correct path
-load_dotenv(f"/mnt/data/dataEng1CW/pipeline/.env")
+
+load_dotenv(f"/mnt/data/dataEng1CW/.env")
+load_dotenv(f"{ROOT_DIR}/.env")
 
 PYTHON3_PATH = os.getenv('PYTHON3_PATH')
 HH_SUITE__BIN_PATH = os.getenv('HH_SUITE__BIN_PATH')
@@ -13,8 +14,28 @@ S4PRED_PATH = os.getenv('S4PRED_PATH')
 BUCKET_NAME = os.getenv('S3_BUCKET_NAME')
 SPARK_MASTER_URL = os.getenv('SPARK_MASTER_URL')
 
-if not (HH_SUITE__BIN_PATH and PDB70_PATH and S4PRED_PATH and PYTHON3_PATH and BUCKET_NAME):
-    print("Please set the paths for S3_BUCKET_NAME, PYTHON3_PATH, HH_SUITE__BIN_PATH, PDB70_PATH and S4PRED_PATH in the .env file")
+if not SPARK_MASTER_URL:
+    print("Please set the SPARK_MASTER_URL in the .env file")
+    sys.exit(1)
+
+if not HH_SUITE__BIN_PATH:
+    print("Please set the HH_SUITE__BIN_PATH in the .env file")
+    sys.exit(1)
+
+if not PDB70_PATH:
+    print("Please set the PDB70_PATH in the .env file")
+    sys.exit(1)
+
+if not S4PRED_PATH:
+    print("Please set the S4PRED_PATH in the .env file")
+    sys.exit(1)
+
+if not PYTHON3_PATH:
+    print("Please set the PYTHON3_PATH in the .env file")
+    sys.exit(1)
+
+if not BUCKET_NAME:
+    print("Please set the BUCKET_NAME in the .env file")
     sys.exit(1)
 
 os.environ['PYSPARK_DRIVER_PYTHON'] = PYTHON3_PATH
---
+++ scripts/setup_local_env.sh
@@ -0,0 +1,15 @@
+DIRECTORY=$(dirname $0)
+
+PYTHON3_PATH=/mnt/data/dataEng1CW/venv/bin/python3
+HH_SUITE__BIN_PATH=/mnt/data/programs/hh-suite/bin
+PDB70_PATH=/mnt/data/pdb70/pdb70
+S4PRED_PATH=/mnt/data/programs/s4pred
+S3_BUCKET_NAME=comp0235-ucabfri
+SPARK_MASTER_URL=spark://ip-10-0-0-169.eu-west-2.compute.internal:7077
+
+echo "PYTHON3_PATH=$PYTHON3_PATH" > $DIRECTORY/../.env
+echo "HH_SUITE__BIN_PATH=$HH_SUITE__BIN_PATH" >> $DIRECTORY/../.env
+echo "PDB70_PATH=$PDB70_PATH" >> $DIRECTORY/../.env
+echo "S4PRED_PATH=$S4PRED_PATH" >> $DIRECTORY/../.env
+echo "S3_BUCKET_NAME=$S3_BUCKET_NAME" >> $DIRECTORY/../.env
+echo "SPARK_MASTER_URL=$SPARK_MASTER_URL" >> $DIRECTORY/../.env
