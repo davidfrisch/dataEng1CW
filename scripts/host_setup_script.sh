259c5dde41855ac18861ce4b47804fdbbcddb8a3
Cloud User <ec2-user@ip-10-0-14-176.eu-west-2.compute.internal>
2023-12-14 16:19:47 +0000

    [ansible] Host install script

---
+++ scripts/host_setup_script.sh
@@ -0,0 +1,57 @@
+#!/bin/bash
+set -e
+DIRECTORY=$(dirname $0)
+
+SECRET_KEY_FILE=""
+
+while [[ "$#" -gt 0 ]]; do
+    case $1 in
+        -s|--secret-key-file) SECRET_KEY_FILE="$2"; shift ;;
+        *) echo "Unknown parameter passed: $1"; exit 1 ;;
+    esac
+    shift
+done
+
+
+if [[ -z "$SECRET_KEY_FILE" ]]; then
+    echo "ERROR: secret key file not specified!"
+    exit 1
+fi
+
+if [[ ! -f "$SECRET_KEY_FILE" ]]; then
+    echo "ERROR: secret key file does not exist!"
+    exit 1
+fi
+
+
+
+python3_version=$(python3 -V 2>&1 | grep -Po '(?<=Python )(.+)')
+
+if [[ $python3_version != 3.9* ]]; then
+    echo "ERROR: this script requires Python3.9!"
+    exit 1
+fi
+
+if ! which pip3 >& /dev/null; then
+    echo "ERROR: pip3 is not installed."
+    echo "       On Ubuntu, run:"
+    echo "       curl -sS https://bootstrap.pypa.io/get-pip.py | sudo python3.9"
+    echo "       and try running this script again."
+    exit 1
+fi
+
+
+echo "
+ssh_private_key: $SECRET_KEY_FILE
+spark_user: ec2-user
+spark_group: ec2-user
+" > $DIRECTORY/../ansible/custom_vars.yml
+
+
+# Software installation
+sudo yum install bind-utils
+python3 -m ensurepip --upgrade
+python3 -m pip install --upgrade pip
+pip3 install ansible
+
+echo "Finished installing host dependencies."
--- scripts/setup_local_env.sh
+++ scripts/setup_local_env.sh
@@ -1,21 +1,58 @@
+#!/bin/bash
+set -e
+
 DIRECTORY=$(dirname $0)
 
+# Path in VMs
 PYTHON3_PATH=/mnt/data/dataEng1CW/venv/bin/python3
 HH_SUITE__BIN_PATH=/mnt/data/programs/hh-suite/bin
 PDB70_PATH=/mnt/data/pdb70/pdb70
 S4PRED_PATH=/mnt/data/programs/s4pred
-SPARK_MASTER_URL=spark://ip-10-0-0-169.eu-west-2.compute.internal:7077
-SPARK_UI_URL="http://ec2-18-130-66-138.eu-west-2.compute.amazonaws.com/spark-master"
-SPARK_LOCAL_UI_URL="http://localhost:8080"
-# DOCKER_SPARK_MASTER_URL=spark://spark-master:7077
-DATABASE_URL="postgresql://postgres:postgres@ip-10-0-0-169.eu-west-2.compute.internal:5432/proteomics"
-DOCKER_DATABASE_URL="postgresql://postgres:postgres@postgres:5432/proteomics"
 SHARE_DIR="/mnt/data/dataEng1CW/data"
 DOCKER_SHARE_DIR="/data"
+
+# For local
+SPARK_LOCAL_UI_URL="http://localhost:8080"
 VITE_BACKEND_URL="http://localhost:3001"
-DOCKER_VITE_BACKEND_URL="http://ec2-18-130-66-138.eu-west-2.compute.amazonaws.com/api"
 FLASK_URL=http://127.0.0.1:5000
 DOCKER_FLASK_URL=http://10.0.0.169:5000
+
+# USER SPECIFIC VARIABLES
+HOSTNAME=""
+DATABASE_NAME=proteomics
+DATABASE_USER=postgres
+DATABASE_PASS=postgres
+
+while [[ "$#" -gt 0 ]]; do
+    case $1 in
+        -h|--hostname) HOSTNAME="$2"; shift ;;
+        -db|--db-name) DATABASE_NAME="$2"; shift ;;
+        -du|--db-user) DATABASE_USER="$2"; shift ;;
+        -dp|--db-pass) DATABASE_PASS="$2"; shift ;;
+        *) echo "Unknown parameter passed: $1"; exit 1 ;;
+    esac
+    shift
+done
+
+if [[ -z "$HOSTNAME" ]]; then
+    echo "ERROR: hostname not specified!, use -h or --hostname"
+    exit 1
+fi
+
+echo "CLIENT HOSTNAME=$HOSTNAME"
+IP_ADDRESS=$(nslookup "$HOSTNAME" | awk '/^Address: / { print $2 }')
+
+if [[ -z "$IP_ADDRESS" ]]; then
+    echo "ERROR: IP address not found in DNS!"
+    exit 1
+fi
+
+SPARK_UI_URL="http://$HOSTNAME/spark-master"
+DOCKER_VITE_BACKEND_URL="http://$HOSTNAME/api"
+DOCKER_DATABASE_URL="postgresql://$DATABASE_USER:$DATABASE_PASS@postgres:5432/$DATABASE_NAME"
+SPARK_MASTER_URL=spark://$IP_ADDRESS:7077
+DATABASE_URL="postgresql://$DATABASE_USER:$DATABASE_PASS@$IP_ADDRESS:5432/$DATABASE_NAME"
+
 
 # For pipeline
 echo "PYTHON3_PATH=$PYTHON3_PATH" > $DIRECTORY/../.env
@@ -54,3 +91,7 @@ echo "FLASK_URL=$DOCKER_FLASK_URL" >> $DIRECTORY/../backend/.env.staging
 # For frontend
 echo "VITE_BACKEND_URL=$DOCKER_VITE_BACKEND_URL" > $DIRECTORY/../frontend/.env.staging
 echo "VITE_SPARK_UI_URL=$SPARK_UI_URL" >> $DIRECTORY/../frontend/.env.staging
+
+
+
+echo "Finished setting up local environment variables."
